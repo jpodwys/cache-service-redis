229747aca1ab86657f647511e1997b412b178082
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-07-25 10:11:46 -0600

    Making sure the refresh object gets an updated expiration.

--- redisCacheModule.js
+++ redisCacheModule.js
@@ -269,7 +269,7 @@ function redisCacheModule(config){
         if(data.expiration - Date.now() < self.backgroundRefreshMinTtl){
           data.refresh(function (err, response){
             if(!err){
-              self.set(key, response, data.lifeSpan);
+              self.set(key, response, data.lifeSpan, data.refresh, noop);
             }
           });
         }
